7e3ada8a8b89e5b7f95ab9a407acc71ba0af9583
momo-Hendy <166969330+momo-Hendy@users.noreply.github.com>
2024-05-18 17:04:09 -0400

    Add files via upload

---
+++ TheTortiseAndTheHare_Project-master/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace TheTortiseAndTheHare_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnChangeTColor = new System.Windows.Forms.Button();
+            this.btnChangeHColor = new System.Windows.Forms.Button();
+            this.rbPictureMode = new System.Windows.Forms.RadioButton();
+            this.rbDrawMode = new System.Windows.Forms.RadioButton();
+            this.tmrRaceSpeed = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbFast = new System.Windows.Forms.RadioButton();
+            this.rbNormal = new System.Windows.Forms.RadioButton();
+            this.rbSlow = new System.Windows.Forms.RadioButton();
+            this.cdChangeColor = new System.Windows.Forms.ColorDialog();
+            this.pbTortoise = new System.Windows.Forms.PictureBox();
+            this.pbHare = new System.Windows.Forms.PictureBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTortoise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHare)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(229, 280);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(149, 55);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 250);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.Location = new System.Drawing.Point(426, 280);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(149, 55);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(655, 280);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(149, 55);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnChangeTColor);
+            this.groupBox1.Controls.Add(this.btnChangeHColor);
+            this.groupBox1.Controls.Add(this.rbPictureMode);
+            this.groupBox1.Controls.Add(this.rbDrawMode);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(827, 308);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 228);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Draw Mode";
+            // 
+            // btnChangeTColor
+            // 
+            this.btnChangeTColor.Location = new System.Drawing.Point(65, 163);
+            this.btnChangeTColor.Name = "btnChangeTColor";
+            this.btnChangeTColor.Size = new System.Drawing.Size(272, 48);
+            this.btnChangeTColor.TabIndex = 3;
+            this.btnChangeTColor.Text = "Change Tortoise Color";
+            this.btnChangeTColor.UseVisualStyleBackColor = true;
+            this.btnChangeTColor.Click += new System.EventHandler(this.btnChangeTColor_Click);
+            // 
+            // btnChangeHColor
+            // 
+            this.btnChangeHColor.Location = new System.Drawing.Point(65, 109);
+            this.btnChangeHColor.Name = "btnChangeHColor";
+            this.btnChangeHColor.Size = new System.Drawing.Size(272, 48);
+            this.btnChangeHColor.TabIndex = 2;
+            this.btnChangeHColor.Text = "Change Hare Color";
+            this.btnChangeHColor.UseVisualStyleBackColor = true;
+            this.btnChangeHColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // rbPictureMode
+            // 
+            this.rbPictureMode.AutoSize = true;
+            this.rbPictureMode.Location = new System.Drawing.Point(32, 72);
+            this.rbPictureMode.Name = "rbPictureMode";
+            this.rbPictureMode.Size = new System.Drawing.Size(109, 33);
+            this.rbPictureMode.TabIndex = 1;
+            this.rbPictureMode.TabStop = true;
+            this.rbPictureMode.Text = "Picture";
+            this.rbPictureMode.UseVisualStyleBackColor = true;
+            // 
+            // rbDrawMode
+            // 
+            this.rbDrawMode.AutoSize = true;
+            this.rbDrawMode.Location = new System.Drawing.Point(32, 33);
+            this.rbDrawMode.Name = "rbDrawMode";
+            this.rbDrawMode.Size = new System.Drawing.Size(90, 33);
+            this.rbDrawMode.TabIndex = 0;
+            this.rbDrawMode.TabStop = true;
+            this.rbDrawMode.Text = "Draw";
+            this.rbDrawMode.UseVisualStyleBackColor = true;
+            // 
+            // tmrRaceSpeed
+            // 
+            this.tmrRaceSpeed.Tick += new System.EventHandler(this.tmrRaceSpeed_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbFast);
+            this.groupBox2.Controls.Add(this.rbNormal);
+            this.groupBox2.Controls.Add(this.rbSlow);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(107, 354);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(187, 170);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Speed Mode";
+            // 
+            // rbFast
+            // 
+            this.rbFast.AutoSize = true;
+            this.rbFast.Location = new System.Drawing.Point(32, 111);
+            this.rbFast.Name = "rbFast";
+            this.rbFast.Size = new System.Drawing.Size(80, 33);
+            this.rbFast.TabIndex = 2;
+            this.rbFast.TabStop = true;
+            this.rbFast.Text = "Fast";
+            this.rbFast.UseVisualStyleBackColor = true;
+            this.rbFast.CheckedChanged += new System.EventHandler(this.rbFast_CheckedChanged);
+            // 
+            // rbNormal
+            // 
+            this.rbNormal.AutoSize = true;
+            this.rbNormal.Location = new System.Drawing.Point(32, 72);
+            this.rbNormal.Name = "rbNormal";
+            this.rbNormal.Size = new System.Drawing.Size(113, 33);
+            this.rbNormal.TabIndex = 1;
+            this.rbNormal.TabStop = true;
+            this.rbNormal.Text = "Normal";
+            this.rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // rbSlow
+            // 
+            this.rbSlow.AutoSize = true;
+            this.rbSlow.Location = new System.Drawing.Point(32, 33);
+            this.rbSlow.Name = "rbSlow";
+            this.rbSlow.Size = new System.Drawing.Size(88, 33);
+            this.rbSlow.TabIndex = 0;
+            this.rbSlow.TabStop = true;
+            this.rbSlow.Text = "Slow";
+            this.rbSlow.UseVisualStyleBackColor = true;
+            this.rbSlow.CheckedChanged += new System.EventHandler(this.rbSlow_CheckedChanged);
+            // 
+            // pbTortoise
+            // 
+            this.pbTortoise.BackColor = System.Drawing.Color.Transparent;
+            this.pbTortoise.Location = new System.Drawing.Point(12, 12);
+            this.pbTortoise.Name = "pbTortoise";
+            this.pbTortoise.Size = new System.Drawing.Size(61, 55);
+            this.pbTortoise.TabIndex = 6;
+            this.pbTortoise.TabStop = false;
+            // 
+            // pbHare
+            // 
+            this.pbHare.Location = new System.Drawing.Point(12, 63);
+            this.pbHare.Name = "pbHare";
+            this.pbHare.Size = new System.Drawing.Size(61, 55);
+            this.pbHare.TabIndex = 7;
+            this.pbHare.TabStop = false;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(438, 465);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(80, 21);
+            this.hScrollBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1217, 559);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.pbHare);
+            this.Controls.Add(this.pbTortoise);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Tortoise v. Hare";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTortoise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHare)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbPictureMode;
+        private System.Windows.Forms.RadioButton rbDrawMode;
+        private System.Windows.Forms.Timer tmrRaceSpeed;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbNormal;
+        private System.Windows.Forms.RadioButton rbSlow;
+        private System.Windows.Forms.RadioButton rbFast;
+        private System.Windows.Forms.Button btnChangeHColor;
+        private System.Windows.Forms.ColorDialog cdChangeColor;
+        private System.Windows.Forms.Button btnChangeTColor;
+        private System.Windows.Forms.PictureBox pbTortoise;
+        private System.Windows.Forms.PictureBox pbHare;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ TheTortiseAndTheHare_Project-master/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheTortiseAndTheHare_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            T1 = new Tortoise();
+            C1 = new Contender();
+            H1 = new Hare();
+        }
+        Contender C1;
+        Tortoise T1;
+        Hare H1;
+
+
+        public class Contender
+        {
+            protected int position;
+            protected int steps;
+            public Color col;
+
+            public Contender()
+            {
+                position = 0;
+                steps = 70;
+                col = Color.Black;
+            }
+
+            public Contender (int iPos, int iStep, Color clr)
+            {
+                position = iPos;
+                steps = iStep;
+                col = clr;
+            }
+
+            public Contender (Contender other)
+            {
+                this.position = other.position;
+                this.steps = other.steps;
+                this.col = other.col;
+            }
+
+            public int getPosition()
+            {
+                return position;
+            }
+
+            public int getSteps()
+            {
+                return steps;
+            }
+
+            public void setposition(int iPosition)
+            {
+                if (iPosition < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    position = iPosition;
+                }
+            }
+
+            public void setsteps(int iSteps)
+            {
+                if (iSteps > position)               
+                    throw new ArgumentOutOfRangeException();                
+                else                
+                    steps = iSteps;                
+            }
+
+            public bool isWinner()
+            {
+                if (position >= steps)                
+                    return true;                
+                else
+                    return false;
+            }
+
+            public void setPositionToZero()
+            {
+                position = 0;
+            }
+        }
+
+        public class Tortoise : Contender
+        {
+            Random rndGenT;
+
+            public Tortoise():base() 
+            {
+                rndGenT = new Random();
+                col = Color.Green;
+
+
+            }
+            public Tortoise(int iPos, int iStep, Color clr, Random rnd)
+                              :base(iPos, iStep, clr)
+            {
+                rndGenT = new Random();
+            }
+
+            public Tortoise(Tortoise other)
+                              : base(other)
+            {
+                rndGenT = new Random();
+            }
+
+            public void  UpdatePosition()
+            {
+                int chance = rndGenT.Next(1000);
+
+                if (chance < 500)
+                    position = position + 3;
+                else if (chance < 700)
+                    position = position - 6;
+                else if (chance < 1000)
+                    position++;
+
+                if (position < 0)
+                    position = 0;
+               
+            }
+
+            public void Draw(Graphics g)
+            {                
+                Brush myBrush = new SolidBrush(col);
+                g.FillEllipse(myBrush, position * 15 + 75, 65, 25, 25);                
+            }            
+        }
+
+        public class Hare : Contender
+        {
+            Random rndGenH;
+
+            public Hare() : base()
+            {
+                rndGenH = new Random();
+                col = Color.Blue;
+            }
+            public Hare(int iPos, int iStep, Color clr, Random rnd) : base(iPos, iStep, clr)
+
+            {
+                rndGenH = new Random();
+            }
+
+            public Hare(Hare other)
+                              : base(other)
+            {
+                rndGenH = new Random();
+            }
+
+            public void UpdatePosition()
+            {
+                int chance = rndGenH.Next(1000);
+
+                if (chance < 200)
+                    position = position + 0;
+                else if (chance < 400)
+                    position = position + 6;
+                else if (chance < 500)
+                    position = position - 7;
+                else if (chance < 700)
+                    position = position + 3;
+                else if (chance < 1000)
+                    position = position - 2;
+
+                if (position < 0)
+                    position = 0;
+                if (position > 70)
+                    position = 70;
+            }
+
+            public void Draw(Graphics g)
+            {
+                Pen mypen = new Pen(col);
+                Brush myBrush = new SolidBrush(col);
+                g.FillRectangle(myBrush, position * 15 + 75, 195, 25, 25);
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrRaceSpeed.Start();
+        }
+
+        private void tmrRaceSpeed_Tick(object sender, EventArgs e)
+        {
+            T1.UpdatePosition();
+            bool Twinner = T1.isWinner();
+            if (Twinner == true)
+            {               
+                tmrRaceSpeed.Stop();
+                MessageBox.Show("TORTOISE WINS!!! YAYYY!!!");
+            }
+
+            H1.UpdatePosition();
+            bool Hwinner = H1.isWinner();
+            if (Hwinner == true)
+            {
+                tmrRaceSpeed.Stop();
+                MessageBox.Show("something");
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            T1.Draw(e.Graphics);
+            H1.Draw(e.Graphics);
+            Graphics g = e.Graphics;
+            Pen linePen = new Pen(Color.Black, 10);
+            Pen racePen = new Pen(Color.Black, 2);
+            g.DrawLine(racePen, 75, 78, 1135, 78);
+            g.DrawLine(racePen, 75, 208, 1135, 208);
+            g.DrawLine(linePen, 1130, 78, 1130, 208);
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            tmrRaceSpeed.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {            
+            T1.setPositionToZero();
+            H1.setPositionToZero();
+            pictureBox1.Invalidate();
+        }
+
+        private void btnChangeColor_Click(object sender, EventArgs e)
+        {
+            cdChangeColor.ShowDialog();            
+            H1.col = cdChangeColor.Color;
+            pictureBox1 .Invalidate();
+        }
+
+        private void btnChangeTColor_Click(object sender, EventArgs e)
+        {
+            cdChangeColor.ShowDialog();
+            T1.col = cdChangeColor.Color;
+            pictureBox1.Invalidate();
+        }
+
+        private void rbSlow_CheckedChanged(object sender, EventArgs e)
+        {
+            tmrRaceSpeed.Interval = 1000;
+        }
+
+        private void rbFast_CheckedChanged(object sender, EventArgs e)
+        {
+            tmrRaceSpeed.Interval = 10;
+        }
+    }
+}
